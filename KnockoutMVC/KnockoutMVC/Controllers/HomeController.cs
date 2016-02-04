a4aaaec0d5b2b5f60967efd32b60a849134bd990
nikkiLuan <moccababy777@gmail.com>
2016-02-04 14:21:25 +1300

    Basic knockoutjs
    EF Code-First

---
+++ KnockoutMVC/KnockoutMVC/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KnockoutMVC.Models;
+
+namespace KnockoutMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Basic()
+        {
+            return View();
+        }
+
+        public ActionResult Advanced()
+        {
+            var person = new Person
+            {
+                FirstName = "Mocca",
+                LastName = "M"
+            };
+
+            return View(person);
+        }
+    }
+}
