efd8f8a71588ca9e843b78f8fa9d937a1a3d3a6c
nikkiLuan <moccababy777@gmail.com>
2016-02-04 15:24:09 +1300

    HtmlHelper Extensions
    HtmlConvertToJson.
    Delete namespace from extensions file, otherwise it won't be available
    in the view.

---
+++ KnockoutMVC/KnockoutMVC/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.Web.Mvc;
+using KnockoutMVC.DAL;
+
+
+    public static class HtmlHelperExtensions
+    {
+        public static HtmlString HtmlConvertToJson(this HtmlHelper htmlHelper, object model)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                Formatting = Formatting.Indented
+            };
+
+            return new HtmlString(JsonConvert.SerializeObject(model, settings));
+        }
+    }
